680d3270e485bfeba40fa25b9bd48887dbc70f02
Aziuria <anecdotia@gmail.com>
2025-04-15 13:45:52 +0300

    Started a mod

--- src/main/java/net/Aziuria/aziuriamod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/Aziuria/aziuriamod/worldgen/ModPlacedFeatures.java
@@ -7,8 +7,6 @@ import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 
