5425e96cca6c1d53980444c7a76eac704cbd5677
Aziuria <anecdotia@gmail.com>
2025-07-02 02:24:05 +0300

    rendering on woodcutterbench and minerbench sorted aswell jeesus it was a pain

--- src/main/java/net/Aziuria/aziuriamod/block/entity/renderer/MinerBenchRenderer.java
+++ src/main/java/net/Aziuria/aziuriamod/block/entity/renderer/MinerBenchRenderer.java
@@ -1,6 +1,8 @@
 package net.Aziuria.aziuriamod.block.entity.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.Aziuria.aziuriamod.block.MinerBenchBlock;
+import net.Aziuria.aziuriamod.block.MinerPart;
 import net.Aziuria.aziuriamod.block.entity.MinerBenchBlockEntity;
 import net.Aziuria.aziuriamod.block.entity.WoodcutterBenchBlockEntity;
 import net.minecraft.client.Minecraft;
@@ -8,6 +10,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class MinerBenchRenderer implements BlockEntityRenderer<MinerBenchBlockEntity> {
@@ -22,6 +25,27 @@ public class MinerBenchRenderer implements BlockEntityRenderer<MinerBenchBlockEn
     public void render(MinerBenchBlockEntity tileEntity, float partialTicks, PoseStack poseStack,
                        MultiBufferSource bufferSource, int combinedLight, int combinedOverlay) {
         BlockState state = tileEntity.getBlockState();
+        Direction facing = state.getValue(MinerBenchBlock.FACING);
+        MinerPart part = state.getValue(MinerBenchBlock.PART);
+
+        double offset = 0.001;
+
+        switch (facing) {
+            case NORTH, SOUTH -> {
+                // For north/south facing, offset X for sides and Z for front/back
+                double xOffset = (part == MinerPart.LEFT) ? -offset : offset;
+                double zOffset = (part == MinerPart.LEFT) ? offset : -offset;
+                poseStack.translate(xOffset, 0, zOffset);
+            }
+            case EAST, WEST -> {
+                // For east/west facing, offset Z for sides and X for front/back
+                double zOffset = (part == MinerPart.LEFT) ? -offset : offset;
+                double xOffset = (part == MinerPart.LEFT) ? offset : -offset;
+                poseStack.translate(xOffset, 0, zOffset);
+            }
+            default -> {}
+        }
+
         blockRenderer.renderSingleBlock(state, poseStack, bufferSource, combinedLight, combinedOverlay);
     }
 }
--- src/main/java/net/Aziuria/aziuriamod/block/entity/renderer/WoodcutterBenchRenderer.java
+++ src/main/java/net/Aziuria/aziuriamod/block/entity/renderer/WoodcutterBenchRenderer.java
@@ -1,12 +1,18 @@
 package net.Aziuria.aziuriamod.block.entity.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.Aziuria.aziuriamod.block.MinerBenchBlock;
+import net.Aziuria.aziuriamod.block.MinerPart;
+import net.Aziuria.aziuriamod.block.WoodcutterBenchBlock;
+import net.Aziuria.aziuriamod.block.WoodcutterPart;
+import net.Aziuria.aziuriamod.block.entity.MinerBenchBlockEntity;
 import net.Aziuria.aziuriamod.block.entity.WoodcutterBenchBlockEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class WoodcutterBenchRenderer implements BlockEntityRenderer<WoodcutterBenchBlockEntity> {
@@ -21,6 +27,25 @@ public class WoodcutterBenchRenderer implements BlockEntityRenderer<WoodcutterBe
     public void render(WoodcutterBenchBlockEntity tileEntity, float partialTicks, PoseStack poseStack,
                        MultiBufferSource bufferSource, int combinedLight, int combinedOverlay) {
         BlockState state = tileEntity.getBlockState();
+        Direction facing = state.getValue(WoodcutterBenchBlock.FACING);
+        WoodcutterPart part = state.getValue(WoodcutterBenchBlock.PART);
+
+        double offset = 0.001;
+
+        switch (facing) {
+            case NORTH, SOUTH -> {
+                double xOffset = (part == WoodcutterPart.LEFT) ? -offset : offset;
+                double zOffset = (part == WoodcutterPart.LEFT) ? offset : -offset;
+                poseStack.translate(xOffset, 0, zOffset);
+            }
+            case EAST, WEST -> {
+                double zOffset = (part == WoodcutterPart.LEFT) ? -offset : offset;
+                double xOffset = (part == WoodcutterPart.LEFT) ? offset : -offset;
+                poseStack.translate(xOffset, 0, zOffset);
+            }
+            default -> {}
+        }
+
         blockRenderer.renderSingleBlock(state, poseStack, bufferSource, combinedLight, combinedOverlay);
     }
 }
